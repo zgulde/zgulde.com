50652bdfc92cf373ea95b20f8543a5a85fe413ae
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-01 01:51:38 -0500

    added js and css to create the docking navbar, also had to refactor some html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("hello, world!");
+$(document).ready(function(){
+	$body = $('body');
+	$window = $(window);
+	navOffsetTop = $('#navbar').offset().top;
+
+
+	function resize() {
+		$body.removeClass('navbar-docked');
+		navOffsetTop = $nav.offset().top;
+		onScroll();
+	}
+
+	function onScroll() {
+		if(navOffsetTop < $window.scrollTop() && !$body.hasClass('navbar-docked')) {
+			$body.addClass('navbar-docked');
+		}
+		if(navOffsetTop > $window.scrollTop() && $body.hasClass('navbar-docked')) {
+			$body.removeClass('navbar-docked');
+		}
+	}
+
+	$window.on('scroll', onScroll);
+    $window.on('resize', resize);
+});
